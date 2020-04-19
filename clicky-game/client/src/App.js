4d593c30cbe6c563428cc9bb62e56352cc7f960f
Andrew Hardemon <aeroatlas@ymail.com>
2020-04-19 15:43:46 -0700

    small fixes to jumbotron and deleted some unused code

--- clicky-game/client/src/App.js
+++ clicky-game/client/src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LogIn from "./pages/LogIn";
 import Game from "./pages/Game";
 import GameOver from "./pages/GameOver";
-// import Score from "./pages/Score";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import Images from "./components/Images";
 
 class App extends React.Component{
   state = {
@@ -28,12 +26,9 @@ class App extends React.Component{
           <Switch>
             <Route exact path="/" component={LogIn} />\
             <Route exact path="/game" component={Game} />
-            {/* <Route exact path="/game/:id" component={Game} /> */}
             <Route exact path="/gameover" component={GameOver} />
             <Route component={NoMatch} />
           </Switch>
-          {/* <button onClick={this.setTheState}>{mountGame ? "Unm" : "M"}ount Game</button>
-          {mountGame && <Images />} */}
         </div>
       </Router>
     );
